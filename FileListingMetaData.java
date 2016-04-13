b8dca0ebb7407d87661c2ec3b56e984717f58a84
Andreas Stenger <regnets@users.noreply.github.com>
2016-04-13 10:07:19 +0200

    Initial Commit
    These are just the files for the file handling on the viewer side, theses files aren't responsible for getting the images from the camera nor displaying the images in a fullscreen window.

---
+++ FileListingMetaData.java
@@ -0,0 +1,9 @@
+class FileListingMetaData {
+	String filePath;
+	int count;
+
+	public FileListingMetaData(String filePath, int count) {
+		this.filePath = filePath;
+		this.count = count;
+	}
+}
---
+++ FileListingMetaDataChecker.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class FileListingMetaDataChecker {
+	private ArrayList<String> validExtensions;
+
+	public FileListingMetaDataChecker() {
+		validExtensions = new ArrayList<String>();
+	}
+
+	public void addValidExtension(String extension) {
+		if (!validExtensions.contains(extension)) {
+			validExtensions.add(extension);
+		}
+	}
+
+	public boolean hasValidExtension(String filePath) {
+		String extension = "";
+
+		int i = filePath.lastIndexOf('.');
+		int p = Math.max(filePath.lastIndexOf('/'), filePath.lastIndexOf('\\'));
+
+		if (i > p) {
+			extension = filePath.substring(i + 1);
+			if (validExtensions.contains(extension)) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+}
